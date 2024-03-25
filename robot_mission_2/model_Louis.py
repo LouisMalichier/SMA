dc4ef067102fccf31cec778190d5c7ee1060371d
LouisMalichier <louismalichier@student-cs.fr>
2024-03-25 17:32:19 +0100

    changement de façon de faire avec les dossiers

--- robot_mission_2/model_Louis.py
+++ robot_mission_2/model_Louis.py
@@ -1,6 +1,6 @@
 import mesa
 
-from .agents import robot, waste
+from .agents import Robot, Waste
 from .schedule import RandomActivationByTypeFiltered
 
 
@@ -30,8 +30,8 @@ class environment(mesa.Model):
         self.grid = mesa.space.MultiGrid(self.width, self.height, torus=True)
         self.datacollector = mesa.DataCollector(
             {
-                "Wolves": lambda m: m.schedule.get_type_count(robot),
-                "Sheep": lambda m: m.schedule.get_type_count(waste),
+                "Wolves": lambda m: m.schedule.get_type_count(Robot),
+                "Sheep": lambda m: m.schedule.get_type_count(Waste),
             }
         )
 
@@ -40,16 +40,16 @@ class environment(mesa.Model):
         for i in range(self.initial_robot):
             x = self.random.randrange(self.width)
             y = self.random.randrange(self.height)
-            robot_ = robot() # Voir les paramètres de la class robot
-            self.grid.place_agent(robot_, (x, y))
-            self.schedule.add(robot_)
+            robot = Robot() # Voir les paramètres de la class robot
+            self.grid.place_agent(robot, (x, y))
+            self.schedule.add(robot)
 
         for i in range(self.initial_waste):
             x = self.random.randrange(self.width)
             y = self.random.randrange(self.height)
-            waste_ = waste() # Voir les paramètres de la class robot
-            self.grid.place_agent(waste_, (x, y))
-            self.schedule.add(waste_)
+            waste = Waste() # Voir les paramètres de la class robot
+            self.grid.place_agent(waste, (x, y))
+            self.schedule.add(waste)
 
         self.running = True
         self.datacollector.collect(self)
@@ -62,5 +62,5 @@ class environment(mesa.Model):
     def run_model(self):
         # Pas de self.step_count dans notre projet, mais plutot on continue de s'executer tant qu'il reste des dechets sur la grille, donc une condition en while sur waste
 
-        while self.schedule.get_type_count(waste) != 0 : # Syntaxe à verifier mais exemple
+        while self.schedule.get_type_count(Waste) != 0 : # Syntaxe à verifier mais exemple
             self.step()
