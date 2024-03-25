eae4d1b4163f82a6f2f8988208dbf7618f6209a4
nicolas <nicolas.saudreau@student-cs.fr>
2024-03-25 17:21:55 +0100

    Premier model

--- robot_mission_2/agent_Nicolas.py
+++ robot_mission_2/agent_Nicolas.py
@@ -6,30 +6,91 @@ class V0Agent(Agent):
     """ An agent with no waste"""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.waste = 0
+        self.inventory = []
 
     def move(self):
         possible_positions = self.model.grid.get_neighborhood(
-            self.pos)
-            #moore=True,
-            #include_center=False)
+            self.pos
+            moore=False,
+            include_center=False
+        )
         new_position = self.random.choice(possible_positions)
         self.model.grid.move_agent(self, new_position)
 
+    '''''
     def transform(self): 
         transformed = 0
         if self.waste ==1 : 
             transformed =+1 
             self.waste = self.waste - 1
-
-    def give_money(self):
+    '''
+            
+    def pick_up_waste(self):
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        if len(cellmates) > 1:
-            other = self.random.choice(cellmates)
-            other.wealth += 1
-            self.wealth -= 1
+        waste = [obj for obj in cellmates if isinstance(obj, WasteAgent)]
+        if waste:
+            target = waste[0]
+            self.inventory.append(target)
+            self.model.grid.remove_agent(target)
+
 
     def step(self):
-        self.move()
-        if self.wealth > 0:
-            self.give_money()
+        if self.robot_type == "Green":
+            if len(self.inventory) < 2:
+                self.pick_up_waste()
+            else:
+                self.transform_waste()
+        elif self.robot_type == "Yellow":
+            if len(self.inventory) < 2:
+                self.pick_up_waste()
+            else:
+                self.transform_waste()
+        elif self.robot_type == "Red":
+            if len(self.inventory) < 1:
+                self.pick_up_waste()
+            else:
+                self.transport_waste()
+
+
+    def transport_waste(self):
+        if self.robot_type == "Green":
+            pass  # Green robots cannot transport waste
+        elif self.robot_type == "Yellow":
+            if len(self.inventory) >= 1 and self.model.grid.width > self.pos[0] + 1:
+                self.inventory = []  # Consume 1 red waste
+                self.move()
+        elif self.robot_type == "Red":
+            if len(self.inventory) >= 1 and self.model.grid.width > self.pos[0] + 1:
+                self.inventory = []  # Consume 1 red waste
+                self.move()
+
+
+    # def give_money(self):
+    #     cellmates = self.model.grid.get_cell_list_contents([self.pos])
+    #     if len(cellmates) > 1:
+    #         other = self.random.choice(cellmates)
+    #         other.wealth += 1
+    #         self.wealth -= 1
+
+    # def step(self):
+    #     self.move()
+    #     if self.wealth > 0:
+    #         self.give_money()
+
+class WasteAgent(Agent):
+    def __init__(self, unique_id, model, category, position, number):
+        super().__init__(unique_id, model)
+        self._category = None
+        self.category = category
+        self.position = position
+        self.position = number
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if value not in ["green", "yellow", "red"]:  # Remplacez "value1", "value2", "value3" par les valeurs autorisées
+            raise ValueError("Category must be one of ['green', 'yellow', 'red']")
+        self._category = value
